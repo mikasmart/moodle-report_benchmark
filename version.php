e81e9b5f8cca7b7bfafde68d51f6a3ea90136a02
@Mika <mickael.pannequin@smartcanal.com>
2016-06-26 03:41:03 +0200

    Readme Update

--- version.php
+++ version.php
@@ -33,6 +33,5 @@ defined('MOODLE_INTERNAL') || die;
 // Plugin informations
     $plugin->requires   = 2011120500; // Requires this Moodle version  2.0
     $plugin->version    = 2016062600; // The current module version (Date: YYYYMMDDXX)
-    $plugin->maturity   = MATURITY_BETA;
-    $plugin->release    = 'v0.9-r1';
     $plugin->component  = 'report_benchmark'; // Full name of the plugin (used for diagnostics)
+    $plugin->maturity   = MATURITY_BETA;
